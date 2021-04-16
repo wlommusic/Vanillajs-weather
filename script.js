efd3fa3b690ce74ce618f266e7fe0afa3c0bb672
Dipta ranjan panda <64889320+wlommusic@users.noreply.github.com>
2021-04-16 19:30:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ const key = "RpEuZh5DzEaXzvJAVflQaZyyARww0NwG";
 // get weather information
 const getWeather = async (id) => {
   const base =
-    "http://dataservice.accuweather.com/currentconditions/v1/";
+    "https://dataservice.accuweather.com/currentconditions/v1/";
   const query = `${id}?apikey=${key}`;
 
   const response = await fetch(base + query);
@@ -78,7 +78,7 @@ const getWeather = async (id) => {
 // get city information
 const getCity = async (city) => {
   const base =
-    "http://dataservice.accuweather.com/locations/v1/cities/search";
+    "https://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
 
   const response = await fetch(base + query);
